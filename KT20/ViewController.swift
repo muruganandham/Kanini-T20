45fe96704577a78b052b69e7f20907b5f3b64c7a
Muruganandham K <muruganandham23@gmail.com>
2020-10-29 22:30:17 +0530

    Initial commit

---
+++ KT20/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KT20
+//
+//  Created by Muruganandham on 29/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
