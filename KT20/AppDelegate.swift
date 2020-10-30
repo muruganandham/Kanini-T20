5ad1cc3c3aee9f036072a03837685b7f6194c319
EzhilAdhavan <ezhil.adhavan@kanini.com>
2020-10-30 16:36:45 +0530

    * Firebase configuration

--- KT20/AppDelegate.swift
+++ KT20/AppDelegate.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import Firebase
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
+        var ref: DatabaseReference!
+
+        ref = Database.database().reference()
+        print(ref)
         return true
     }
 
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'KT20' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for KT20
+  pod 'Firebase/Database'
+
+  target 'KT20Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'KT20UITests' do
+    # Pods for testing
+  end
+
+end
