7d093adccbb9643a2a3d526ed1b030310d3efbc5
Muruganandham K <muruganandham23@gmail.com>
2020-10-30 19:31:47 +0530

    Project setup

--- KT20/SplashViewController.swift
+++ KT20/SplashViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SplashViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // MARK: - Add User defined methods
 }
 
