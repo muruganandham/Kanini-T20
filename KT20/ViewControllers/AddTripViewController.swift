6d1c454cfbeb7402e26fe2bec80330aecd969355
Muruganandham K <muruganandham23@gmail.com>
2020-10-31 12:28:43 +0530

    Project setup and UI enhancements

--- KT20/ViewControllers/AddTripViewController.swift
+++ KT20/ViewControllers/AddTripViewController.swift
@@ -8,22 +8,61 @@
 import UIKit
 
 class AddTripViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var startButton: UIButton! {
+        didSet {
+            startButton.layer.cornerRadius = startButton.frame.size.height / 2.0
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var stopButton: UIButton! {
+        didSet {
+            stopButton.layer.cornerRadius = stopButton.frame.size.height / 2.0
+        }
     }
-    */
-
+    
+    @IBOutlet weak var addSpotButton: UIButton! {
+        didSet {
+            addSpotButton.layer.cornerRadius = addSpotButton.frame.size.height / 2.0
+        }
+    }
+    
+    var isStarted: Bool = false {
+        didSet {
+            if isStarted {
+                startButton.isHidden = true
+                addSpotButton.isHidden = false
+                stopButton.isHidden = false
+            } else {
+                startButton.isHidden = false
+                addSpotButton.isHidden = true
+                stopButton.isHidden = true
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        isStarted = false
+    }
+    
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        
+        if isStarted {
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func startButtonPressed(_ sender: Any) {
+        isStarted = true
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: Any) {
+        isStarted = false
+    }
+    
+    @IBAction func addStopButtonPressed(_ sender: Any) {
+    }
 }
