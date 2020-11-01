e33e265d61b7e08b8ebfa42c471a357a8d9d1930
Muruganandham K <muruganandham23@gmail.com>
2020-11-01 21:24:31 +0530

    fix drawline wip

--- KT20/ViewControllers/SpotsViewController.swift
+++ KT20/ViewControllers/SpotsViewController.swift
@@ -24,14 +24,29 @@ class SpotsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("routeDict: \(routeDict)")
-
+        
         var points: [CLLocationCoordinate2D] = []
         var center: CLLocationCoordinate2D?
-        routeDict?.forEach({ (point) in
+        
+        let sortedArray = routeDict?.sorted(by: { item1, item2 in
+            
+            let dict1 = item1.value as! Dictionary<String, Any>
+            let dict2 = item2.value as! Dictionary<String, Any>
+            
+            print("*****************")
+            print("**\(dict1["createdAt"])")
+            let string: String = "\(dict1["createdAt"])"
+            print("**\(DateFormatter.iso8601Full.date(from: string))")
+            print("*****************")
+
+            return true
+        })
+        
+        sortedArray?.forEach({ (point) in
             
             let latLong: Dictionary = point.value as! Dictionary<String, Any>
             print("latLong: \(latLong)")
- 
+            
             let lat: NSNumber = latLong["lat"]! as! NSNumber
             let lng: NSNumber = latLong["lng"]! as! NSNumber
             
@@ -40,31 +55,39 @@ class SpotsViewController: UIViewController {
             print(lng)
             
             
-            if(points.count < 10) {
             let point = CLLocationCoordinate2DMake(lat.doubleValue, lng.doubleValue);
             if(center == nil) {
                 center = point
             }
+            
             points.append(point)
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = point
+            annotation.title = "\(point.latitude), \(point.longitude)"
+            mapView.addAnnotation(annotation)
+            
             print("point: \(point)")
-            }
         })
         
         print("points: \(points)")
         
-//        let point1 = CLLocationCoordinate2DMake(37.42261989, -122.22622172);
-//        let point2 = CLLocationCoordinate2DMake(37.41883236, -122.2175664);
-//        let point3 = CLLocationCoordinate2DMake(37.41926445, -122.22021307);
-//        //let point4 = CLLocationCoordinate2DMake(37.42384553, -122.22799483);
-//
-//        points.append(point1)
-//        points.append(point2)
-//        points.append(point3)
-//        //points.append(point4)
-//        center = point1
+        //        let point1 = CLLocationCoordinate2DMake(37.42261989, -122.22622172);
+        //        let point2 = CLLocationCoordinate2DMake(37.41883236, -122.2175664);
+        //        let point3 = CLLocationCoordinate2DMake(37.41926445, -122.22021307);
+        //        //let point4 = CLLocationCoordinate2DMake(37.42384553, -122.22799483);
+        //
+        //        points.append(point1)
+        //        points.append(point2)
+        //        points.append(point3)
+        //        //points.append(point4)
+        //        center = point1
         
-//        let geodesic = MKGeodesicPolyline(coordinates: points, count: points.count)
-//        mapView.addOverlay(geodesic)
+        //        let geodesic = MKGeodesicPolyline(coordinates: points, count: points.count)
+        //        mapView.addOverlay(geodesic)
+        
+        let overlays = mapView.overlays
+        mapView.removeOverlays(overlays)
         
         let polyline = MKPolyline(coordinates: points, count: points.count)
         mapView?.addOverlay(polyline)
